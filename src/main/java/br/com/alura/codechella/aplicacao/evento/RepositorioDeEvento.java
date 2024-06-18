2de080194805d3f0bdda83000eec94edbd567928
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-06-17 21:04:54 -0300

    Serviços de domínio

---
+++ src/main/java/br/com/alura/codechella/aplicacao/evento/RepositorioDeEvento.java
@@ -0,0 +1,10 @@
+package br.com.alura.codechella.aplicacao.evento;
+
+import br.com.alura.codechella.dominio.evento.Endereco;
+import br.com.alura.codechella.dominio.evento.Evento;
+
+public class RepositorioDeEvento {
+    Evento buscarEventoPorCidade(Endereco cep) {
+        return null;
+    }
+}
---
+++ src/main/java/br/com/alura/codechella/aplicacao/usuario/EnviarCupomDesconto.java
@@ -0,0 +1,8 @@
+package br.com.alura.codechella.aplicacao.usuario;
+
+import br.com.alura.codechella.infra.usuario.Usuario;
+
+public class EnviarCupomDesconto {
+
+    void enviarPara(Usuario usuario) {};
+}
